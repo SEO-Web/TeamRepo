a1f0c758a6336b1ce40cf453ee2a660afe036118
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-22 15:02:34 +1030

    modal added to home page for search of city / javascript added to make modal work and when pressed will move to pub-search page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
+// button to open nav bar on smaller screens
+var btn = document.querySelector("button.mobile-menu-button");
+var menu = document.querySelector(".mobile-menu");
 
-const btn = document.querySelector("button.mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
+// modal element selectors
+var mapModal = document.getElementById("map-modal");
+var modalBTN = document.getElementById("modal-btn");
+var searchBTN = document.getElementById("search-btn");
 
+//Modal search value
+//var location = document.getElementById("location").value
+
+/* Need code so that uses the value of search input  */
+
+// Open modal when button is clicked
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+modalBTN.addEventListener("click", function () {
+    mapModal.style.display = "block";
+})
+
+// Close modal when ok clicked
+searchBTN.addEventListener("click", function () {
+    document.location.href = "pub-search.html"
+})
