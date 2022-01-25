e425abdfa76c68843442b2b4e5d63d93ac9ecd67
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-25 19:11:37 +1030

    session storage test

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,7 @@ var modalBTN = document.getElementById("modal-btn");
 var searchBTN = document.getElementById("search-btn");
 var formEl = document.getElementById("city-search");
 var closeEl = document.getElementById("closeBtn");
-var ageModal = document.getElementById("age-modal");
-var noBtn = document.getElementById("no-btn");
-var yesBtn = document.getElementById("yes-btn");
-var tooYoung = document.getElementById("underage");
-var mainSection = document.getElementById("main-section")
+
 
 /* Need code so that uses the value of search input  */
 
@@ -42,17 +38,4 @@ function locationSearchTerm(event) {
   
 formEl.addEventListener("submit", locationSearchTerm);
 
-yesBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    ageModal.style.display = "none";
-    mainSection.style.display = "block";
-})
-
-noBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    var warning = document.createElement("p");
-    warning.classList.add("mt-3", "leading-6", "font-medium", "text-gray-900")
-    warning.textContent = "You are too young to enter the website.";
-    tooYoung.append(warning);
-})
 
